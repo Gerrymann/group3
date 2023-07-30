9967c26feaef808dcc879c514f4c11c7207f1949
Gerrymann <136470631+Gerrymann@users.noreply.github.com>
2023-07-30 20:27:34 +0200

    Add files via upload

---
+++ FilmyProjekt/FilmyProjekt/filmy/data_herci.py
@@ -0,0 +1,10 @@
+herci_data = {
+   'tom-hanks': {
+       'nazev' : 'Tom Hanks',
+       'popis' : 'Thomas Jeffrey  Hanks (* 9. července 1956 Concord) je americký filmový herec, režisér a producent, držitel dvou Oscarů za herecký výkon. Jedná se o jednoho z nejlépe placených herců v dějinách filmu a jednoho ze dvou herců (vedle Spencera Tracyho), který cenu akademie obdržel ve dvou po sobě jdoucích letech (1993 a 1994). ',
+   },
+    'tom-cruise' : {
+        'nazev' : 'Tom Cruis',
+        'popis' : 'Thomas "Tom" Cruise Mapother IV (* 3. července 1962 Syracuse, New York, USA) je americký herec, scenárista, producent a režisér.',
+    },
+}
---
+++ FilmyProjekt/FilmyProjekt/filmy/views.py
@@ -0,0 +1,67 @@
+
+from django.shortcuts import render, HttpResponse, Http404
+from django.http import JsonResponse
+from django.shortcuts import reverse
+from django.utils.text import slugify
+from filmy.data import filmy_data
+from filmy.data_herci import herci_data
+
+
+# Create your views here.
+def seznam(request):
+    slug = slugify(request.GET.get('text', ''))
+
+    if slug:
+        data = {}
+        for key_film in filmy_data:
+            if key_film.startswith(slug):
+                data[key_film] = filmy_data[key_film]
+
+
+        context = {'filmy_data': data}
+    else:
+        context = {'filmy_data': filmy_data}
+
+    return render(request, 'filmy/seznam.html', context=context)
+
+def detail(request, slug):
+    if slug in filmy_data:
+        film = filmy_data[slug]
+        response = render(request, 'filmy/detail.html', context={'film': film})
+        response['MOJE-DATA'] = 'HELLO'
+        #response['Content-Type'] = 'application/json; charset=utf-8'
+        return response
+    else:
+        raise Http404('Film nebyl nalezen')
+
+def seznam_json(request):
+    return JsonResponse(filmy_data)
+
+def seznam_hercu(request):
+    return render(request, 'filmy/seznam_herci.html', context={'herci': herci_data})
+
+def detail_herce(request, slug):
+    if slug in herci_data:
+        herec = herci_data[slug]
+        return render(request, 'filmy/detail_herci.html', context={'herec' : herec})
+    else:
+        raise Http404 ("Herec nebyl nalezen")
+
+
+
+"""
+0) mít data v dict
+1) views.py
+seznam_hercu -> return render() + html
+detail_herce -> return render() + html
+
+2) urls.py
+/seznam_herci/
+-> /filmy/seznam-hercu/
+
+/detail-herce/<slug:slug>/
+-> /filmy/detail-herce/jim_carrey/
+
+
+3) nedělat search
+"""
