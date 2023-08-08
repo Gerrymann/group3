263e4f5fb3d5fed0a6af1f1b904500d27c2295ba
Gerrymann <136470631+Gerrymann@users.noreply.github.com>
2023-08-08 22:21:00 +0200

    Add files via upload

---
+++ EShop_project/eshop/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from eshop import models
+
+
+admin.site.register(models.Kategorie)
+admin.site.register(models.Znacka)
+admin.site.register(models.Produkt)
+admin.site.register(models.Cena)
